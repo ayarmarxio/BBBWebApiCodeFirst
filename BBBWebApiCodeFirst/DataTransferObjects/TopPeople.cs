d3098c7620b1549480c2d570bd7dfaa8f76b94c7
ayarmarxio <ayarmarxio@gmail.com>
2018-11-13 15:04:21 +0100

    DTO Top People

---
+++ BBBWebApiCodeFirst/DataTransferObjects/TopPeople.cs
@@ -0,0 +1,23 @@
+﻿using NetTopologySuite.Geometries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BBBWebApiCodeFirst.DataTransferObjects
+{
+    public class TopPeople
+    {
+        public int Gid { get; set; }
+
+        public int Id { get; set; }
+
+        public int ZoneAct { get; set; }
+
+        public int People { get; set; }
+
+        public Geometry Geom { get; set; }
+        
+
+    }
+}
